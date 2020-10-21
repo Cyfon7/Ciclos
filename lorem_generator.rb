c0d1767ba1a3653980202eda081bfecd2d122baa
Jorge <jorgesifontes.cl@gmail.com>
2020-10-20 21:36:04 -0300

    Final lorem_generator.rb

---
+++ lorem_generator.rb
@@ -0,0 +1,7 @@
+puts "Bienvenido"
+puts "Cuantos parrafos Lorem ipsum desea?"
+n = gets().to_i
+
+n.times do
+    puts "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus viverra vulputate. Fusce consectetur accumsan fringilla. Sed risus nibh, sagittis vitae vestibulum vitae, sodales sed eros. Cras ac neque justo. Pellentesque feugiat eleifend odio eget fringilla. Nulla facilisi. Suspendisse nisi felis, fringilla ut interdum vel, commodo sit amet ligula. Maecenas eu porttitor tortor. Maecenas in porttitor ante. Curabitur faucibus ex lacus, sed lobortis ex tincidunt id. Nullam cursus iaculis turpis a faucibus. Praesent quis sapien aliquet, dictum lorem id, tempor lorem. Morbi non nunc facilisis, tincidunt arcu id, cursus quam.\n\n"
+end
